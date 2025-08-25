300bd7a134bfe3491e1b27f9055e27d6aebe3e32
peter-savron <peter.savron03@gmail.com>
2025-08-25 21:59:52 +0200

    Changes to sample classes

--- src/main/java/si/savron/GreetingResource.java
+++ src/main/java/si/savron/GreetingResource.java
@@ -1,16 +1,24 @@
 package si.savron;
 
+import jakarta.annotation.security.PermitAll;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 
 @Path("/hello")
 public class GreetingResource {
 
+    @Inject
+    JsonWebToken jwt;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @PermitAll
     public String hello() {
-        return "Hello from Quarkus REST";
+        return "Hello from Quarkus REST, this is protected.";
     }
+
 }
